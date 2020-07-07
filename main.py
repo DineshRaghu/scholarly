b5800964b0e1de39527d67611fae5e8f3a32d0a7
Dinesh Raghu <dinesh@Dineshs-MacBook-Pro.local>
2020-07-07 20:18:42 +0530

    Fixed FreeProxy usage

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ from fp.fp import FreeProxy
 from datetime import datetime
 from scholarly import scholarly
 
+def set_new_proxy():
+    while True:
+        proxy = FreeProxy(rand=True, timeout=1).get()
+        proxy_works = scholarly.use_proxy(http=proxy, https=proxy)
+        if proxy_works:
+            break
+    print("Working proxy:", proxy)
+    return proxy    
+
+set_new_proxy()
+
 if __name__ == "__main__":
     
-    proxy = FreeProxy(rand=True, timeout=1, country_id=['US', 'CA']).get()  
-    scholarly.use_proxy(http=proxy, https=proxy)
-
     # cache all author details and serialize them to a flat file
     # helps to recompute related publications count without having to re-crawl
     cache = {}
@@ -20,28 +28,64 @@ if __name__ == "__main__":
             cache = pickle.load(fp)
     
     start = datetime.now()
-    #query = "Dinesh Raghu IBM"
     query = "Mausam Indian Institute of Technology Delhi"
 
-    authors = [a for a in scholarly.search_author(query)]
+    authors = []
+    while True:
+        try:
+            authors = [a for a in scholarly.search_author(query)]
+            #print("Retrieved authors")
+            break
+        except Exception as e:
+            print("trying new proxy")
+            set_new_proxy() 
+
+    
     for author_idx, author in enumerate(authors):
-        print("crawling author", str(author_idx+1)+"/"+str(len(authors)))
+        print("\ncrawling author", str(author_idx+1)+"/"+str(len(authors)))
         if author.id not in cache:
-            author = author.fill([])
+            while True:
+                try:
+                    author = author.fill([])
+                    #print("filled author details")
+                    break
+                except Exception as e:
+                    print("trying new proxy")
+                    set_new_proxy()
+            
             publications = {}
             total_publications = len(author.publications)
             for pub_idx, publication in enumerate(author.publications):
-                print("\tcrawling publication", str(pub_idx+1)+"/"+str(total_publications))
-                publication.fill()
+                print("\tcrawling publication", str(pub_idx+1)+"/"+str(total_publications), "\n")
+                while True:
+                    try:
+                        publication.fill()
+                        #print("\tFilled publication details\n")
+                        break
+                    except Exception as e:
+                        print("\ttrying new proxy")
+                        set_new_proxy()
             cache[author.id] = author
         else:
             author = cache[author.id]
-    
+
+    total_conference_count = 0
+    related_conference_count = 0
     # print author basic details and list of conferences published
-    print(author.name, author.affiliation, "(", author.id,")")
     for idx, publication in enumerate(author.publications):
-        if 'conference' in publication.bib:
-            print("\t",publication.bib['conference'])
+        if 'conference' in publication.bib and 'arXiv' not in publication.bib['conference']:
+            total_conference_count += 1
+            conference = publication.bib['conference']
+            if "International Joint Conference on Artificial Intelligence" in conference or "AAAI Conference on Artificial Intelligence" in conference:
+                related_conference_count += 1
+            #print("\t",publication.bib['conference'])
+    
+    print(author.name, ",", author.affiliation, "(", author.id,")")
+    print("h-index:", str(author.hindex))
+    print("Total citations:", str(author.citedby))
+    print("Interests:", author.interests)
+    print("Total publication count", str(total_conference_count))
+    print("Related conference publication count", str(related_conference_count))
 
     end = datetime.now()
     difference = (end - start).seconds
